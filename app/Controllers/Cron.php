590a2a77e42a9a32491d75d1a716df93b0ec7eb2
icdrrrrr <icdrrrrr@icdrrrrrs-MacBook-Air.local>
2022-06-24 16:31:45 +0700

    fix something in cron report

--- app/Controllers/Cron.php
+++ app/Controllers/Cron.php
@@ -12,15 +12,19 @@ class Cron extends BaseController
         // save data to viewer
         $row = $this->viewer->where('v_ua_browser', null)->where('raw_useragent !=', null)->first();
         if(@$row['raw_useragent']) {
+            var_dump($row);
+            var_dump($UA_report);
+
             $UA_report = $Browser->getAll($row['raw_useragent']);
 
-            $this->viewer
+            $status = $this->viewer
                 ->set('v_ua_browser', $UA_report['browser_name'])
                 ->set('v_ua_os', $UA_report['os_name'])
                 ->set('v_ua_device', $UA_report['os_type'])
                 ->where('id', $row['id'])
                 ->update();
-
+            
+            var_dump($status);
         }
 
         return;
